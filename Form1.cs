953c4c5c206a3b6d6ffcae0701a5d58640fe0ba5
Hoang Huy <you@example.com>
2024-01-26 23:15:08 +0700

    Huy chinh o nhanh

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace BaiTapGit
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Huy chinh code o nhanh
+        }
     }
 }
